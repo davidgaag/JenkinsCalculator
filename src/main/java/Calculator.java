423be3d3b0c8609fcf67dc855a3c82f73615d48d
David Gaag <davidgaag3@gmail.com>
2023-03-02 20:31:42 -0700

    Test push #4 for Jenkins

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -22,10 +22,6 @@ class Calculator {
         return a / b;
     }
 
-    void foo() {
-        System.out.println("Hello world!");
-    }
-
     /*
     Returns the n'th number in the fibonacci sequence
     https://en.wikipedia.org/wiki/Fibonacci_number
